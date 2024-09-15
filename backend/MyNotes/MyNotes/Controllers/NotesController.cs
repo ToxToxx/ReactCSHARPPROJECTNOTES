1b875650b9e6f60850c0f20274c4fa2ada43b5f9
Matvey <sinema3d00@gmail.com>
2024-09-15 20:24:40 +0300

    making notes controller

---
+++ backend/MyNotes/MyNotes/Controllers/NotesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyNotes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return View();
+        }
+    }
+}
