7d2abdd1a20fb996730fb18f7be99c00be58fa45
Matvey <sinema3d00@gmail.com>
2024-09-19 14:21:08 +0300

    fetching data from backend

--- frontend/my-notes/src/App.jsx
+++ frontend/my-notes/src/App.jsx
@@ -1,11 +1,19 @@
-
-
+import { useEffect } from 'react';
 import './App.css'
 import CreateNoteForm from './components/CreateNoteForm';
 import Filters from './components/Filters';
 import Note from './components/Note';
+import { fetchNotes } from './services/notes';
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchNotes();
+    }
+
+    fetchData();
+  }, [])
+
   return (
   <section className='p-8 flex flex-row justify-start items-start gap-12'>
     <div className='flex flex-col w-1/3 gap-10'>
