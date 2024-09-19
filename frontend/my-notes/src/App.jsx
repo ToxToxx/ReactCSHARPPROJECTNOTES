a98928fd32f18b34f46238d5ca3355940aac72c6
Matvey <sinema3d00@gmail.com>
2024-09-19 11:32:03 +0300

    making creating note logic

--- frontend/my-notes/src/App.jsx
+++ frontend/my-notes/src/App.jsx
@@ -1,10 +1,16 @@
+import { Button, Input, Textarea } from '@chakra-ui/react';
 import './App.css'
 
 function App() {
-  return (
-    <>
-    </>
-  )
+  return (<section>
+    <div>
+      <form>
+        <h3>Создать заметку</h3>
+        <Input placeholder='Название'/>
+        <Textarea placeholder='Описание'/>
+        <Button>Создать</Button>
+      </form>
+    </div>
+     </section>)
 }
-
 export default App
