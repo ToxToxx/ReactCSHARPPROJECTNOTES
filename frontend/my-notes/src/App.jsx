fcec0f53055eedb17f2a37312c9e5fe62a3a75c5
Matvey <sinema3d00@gmail.com>
2024-09-19 13:49:36 +0300

    separate logic

--- frontend/my-notes/src/App.jsx
+++ frontend/my-notes/src/App.jsx
@@ -1,17 +1,15 @@
 import { Button, Input, Textarea } from '@chakra-ui/react';
 import './App.css'
+import CreateNoteForm from './components/CreateNoteForm';
 
 function App() {
   return (
   <section className='p-8 flex flex-row justify-start items-start gap-12'>
     <div className='flex flex-col w-1/3 gap-10'>
-      <form className='w-full flex flex-col gap-3'>
-        <h3 className='font-bold text-xl'>Создать заметку</h3>
-        <Input placeholder='Название'/>
-        <Textarea placeholder='Описание'/>
-        <Button colorScheme='teal'>Создать</Button>
-      </form>
+      <CreateNoteForm />
     </div>
   </section>)
 }
+
+
 export default App
