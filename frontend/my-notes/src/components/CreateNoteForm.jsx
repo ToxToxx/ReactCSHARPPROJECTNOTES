7fd80185fd36a06abf1f7c1b35ffeca0e3467564
Matvey <sinema3d00@gmail.com>
2024-09-20 13:22:34 +0300

    expanding create note form for creating form logic

--- frontend/my-notes/src/components/CreateNoteForm.jsx
+++ frontend/my-notes/src/components/CreateNoteForm.jsx
@@ -1,11 +1,30 @@
 import { Button, Input, Textarea } from '@chakra-ui/react';
+import { useState } from 'react';
 
-export default function CreateNoteForm() {
+export default function CreateNoteForm({onCreate}) {
+  const [note, setNote] = useState(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setNote(null);
+    onCreate(note);
+  };
+
     return (
-        <form className='w-full flex flex-col gap-3'>
+        <form onSubmit={onSubmit} className='w-full flex flex-col gap-3'>
           <h3 className='font-bold text-xl'>Создать заметку</h3>
-          <Input placeholder='Название'/>
-          <Textarea placeholder='Описание'/>
-          <Button colorScheme='teal'>Создать</Button>
+          <Input 
+          placeholder='Название' 
+          value={note?.title ?? ""}
+          onChange={(e) => setNote({
+              ...note, title: e.target.value
+            })}/>
+          <Textarea 
+          placeholder='Описание' 
+          value={note?.description ?? ""}
+          onChange={(e) => setNote({
+              ...note, description: e.target.value
+            })}/>
+          <Button type="submit" colorScheme='teal'>Создать</Button>
         </form>)
   }
